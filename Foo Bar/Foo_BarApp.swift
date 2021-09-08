9422f08026e8b70f33e4520dd33d38d53c98f166
Natan Humieniuk <nhumieniuk@bsd220.org>
2021-09-08 10:27:23 -0500

    Initial Commit

---
+++ Foo Bar/Foo_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foo_BarApp.swift
+//  Foo Bar
+//
+//  Created by Student on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Foo_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
